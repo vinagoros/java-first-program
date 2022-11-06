8716b95d5d183642e64dfc1649943feb984d9f0c
vinagoros <98480269+vinagoros@users.noreply.github.com>
2022-11-06 23:29:12 +0000

    finished module 2

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,13 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number*2;
+    }
+    private static int add(int[] numbers){
+        int sum = 0;
+        for(int number:numbers){
+            sum += number;
+        }
+        return sum;
     }
 }
