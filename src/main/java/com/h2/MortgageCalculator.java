a184200b918f772668ef94e74f8ad55d931fb6b5
vinagoros <98480269+vinagoros@users.noreply.github.com>
2022-11-09 20:25:34 +0000

    fixed module 6

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -38,7 +38,7 @@ public class MortgageCalculator {
     public static void main(String[] args) {
         long loanAmount = Utilities.getLongValue(args[0]);
         int termInYears = Utilities.getIntValue(args[1]);
-        float annualRate = Float.parseFloat(args[2]);
+        float annualRate = Utilities.getFloatValue(args[2]);
         MortgageCalculator calculator = new MortgageCalculator(loanAmount,termInYears,annualRate);
         calculator.calculateMonthlyPayment();
         System.out.println(calculator.toString());
